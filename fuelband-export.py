67259907df4c6fc9cd1a22c7b1ca3ac1b31a7b14
Mookie Kong <ultramookie@ymail.com>
2014-03-02 21:58:54 -0800

    added fields for emotion and notes. updated formatting.

--- fuelband-export.py
+++ fuelband-export.py
@@ -4,20 +4,17 @@ import json
 import urllib
 import urllib2
 import time
- 
-""" Nike Plus Fuelband Activity Log
-https://developer.nike.com
-based on https://gist.github.com/leah/5655437
-"""
- 
-ACCESS_TOKEN = 'YOUR_NIKE_ACCESS_TOKEN'
+
+ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
  
 base_url = 'https://api.nike.com'
 url = '/me/sport/activities/FUELBAND?access_token=%s&experienceType=FUELBAND' % ACCESS_TOKEN
 headers = {'appid':'fuelband', 'Accept':'application/json'} # weird required headers, blah.
 current_month = None
+emotion = ''
+note = ''
 
-file = open('nike.txt','w+')
+file = open('fuelband.txt','w+')
 
 while url:
  
@@ -47,13 +44,20 @@ while url:
 			calories = metrics.get('calories')
 			fuel = metrics.get('fuel')
 			steps = metrics.get('steps')
- 
+
+			for tag in activity.get('tags'):
+				if tag.get('tagType') == "EMOTION":
+					emotion = tag.get('tagValue').replace('_','-').title()
+				if (tag.get('tagType') == "NOTE") and (tag.get('tagValue') != "NOTE"):
+					note = tag.get('tagValue')
+
 			# remove milliseconds
 			duration = metrics.get('duration').partition('.')[0]
 
 			if activity.get('activityType') == "ALL_DAY": 
-				file.write(date + ' : fuel: ' + str(fuel) + '\tcalories: ' +  str(calories) + '\tactive: '  + duration + ' ' + '\n')
- 
+				file.write(date + ' : fuel: ' + str(fuel).ljust(6) + '\tcalories: ' +  str(calories).ljust(6) + '\tactive: '  + duration.ljust(8) + '\t' + emotion.ljust(12) + '\t' + note + ' ' + '\n')
+				emotion = '' 
+				note = ''
 	# pagination
 	url = None
 	if resp.get('paging') and resp.get('paging').get('next'):
